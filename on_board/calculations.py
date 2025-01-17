e013863fd45156a293fd8a26326b2b45aced5120
John <johnwolniak@proton.me>
2025-01-16 17:33:24 -0700

    Added config file, optional server functionality, and more robust sample difference function

--- on_board/calculations.py
+++ on_board/calculations.py
@@ -9,18 +9,27 @@ def detect_peaks(data, height=0.3, sample_threshold=1000):
         clusters = time_diffs > sample_threshold
         cluster_indices = np.concatenate(([0], np.cumsum(clusters)))
         unique_clusters = np.unique(cluster_indices)
-        peaks = np.array([int(np.mean(peaks[cluster_indices == cluster])) for cluster in unique_clusters])
+        peaks = np.array([np.mean(peaks[cluster_indices == cluster]).astype(int) for cluster in unique_clusters])
     else:
         peaks = np.array([], dtype=int)  # Ensure an empty integer array is returned if no peaks are detected
     return peaks
 
-def calculate_sample_differences(peaks1, peaks2):
-    if len(peaks1) < 1 or len(peaks2) < 1:
+def calculate_sample_differences(t_ref, t_s):
+    # Note: t_ref is the refrence laser, and t_s is the laser to be stabalized
+    if len(t_ref) < 1 or len(t_s) < 1:
         print('Error: One or both peak(s) not detected')
         return 0  # Not enough peaks detected in one or both datasets
     
     # Calculate the difference between the first peak of each dataset
-    sampling_difference = peaks2[0] - peaks1[0]
+    # Ensure that sample difference is the difference between the first t_ref peak to the next t_s peak
+    if t_ref[0] > t_s[0]:
+        try: 
+            sampling_difference = t_s[1] - t_ref[0]
+        except IndexError: 
+            print("Second t_s peak not detected")
+            return None
+    else: 
+        sampling_difference = t_s[0] - t_ref[0]
     return sampling_difference
 
 def calculate_error_correction(sample_differences, target_sample_diff, gain, feedback):
@@ -29,13 +38,10 @@ def calculate_error_correction(sample_differences, target_sample_diff, gain, fee
     try:
         # Calculate the error based on the difference in peaks and the target sample difference
         error = sample_differences - target_sample_diff
-        print(f"sample error: {error}")
         # Update the feedback based on the error and gain
         new_feedback = feedback + gain * error
-        print(f"feedback adj:{new_feedback}")
         # Clamp the feedback to be within -1000 and 1000
         clamped_feedback = np.clip(new_feedback, -1000, 1000)
-        print(f"feedback post clip:{feedback}")
     except TypeError:
         print("Error in calculating error correction. Setting sampling difference to 0.")
     return clamped_feedback
--- on_board/onboard_main.py
+++ on_board/onboard_main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import socket
 import time
 import sys
 import json
@@ -7,74 +8,138 @@ import redpitaya_scpi as scpi
 from scpi_interface import setup_redpitaya, acquire_data, output_feedback_voltage
 from calculations import detect_peaks, calculate_sample_differences, calculate_error_correction
 
-# Use localhost IP for onboard execution
-IP = "127.0.0.1"
-rp_s = scpi.scpi(IP)
+
+# Load configuration from JSON file
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
 
 # Set Parameters
-trigger_level = 0  # Trigger position is at the center of the memory buffer
-decimation = 8
-target_sample_diff = 2000
-gain = .5
-feedback = 0
-    
+trigger_level = config["trigger_level"]
+decimation = config["decimation"]
+target_sample_diff = config["target_sample_diff"]
+gain = config["gain"]
+feedback = config["feedback"]
+use_server = config["use_server"]
+rest_time = config["rest_time"]
+
+# Use localhost IP for onboard execution
+IP = config["IP"]
+rp_s = scpi.scpi(IP)
+
 # Setup Red Pitaya
 setup_redpitaya(rp_s, trigger_level, decimation)
 
-try:
-    # Set up socket communication
-    import socket
-    SERVER_IP = ''  # Replace with your server's IP (User Ethernet Adapter, and not Ethernet 4)
-    SERVER_PORT = 65432
+if use_server:
+    print("Server functionality is enabled")
+    try:
+        # Set up socket communication
+        SERVER_IP = ''  # Replace with your server's IP (User Ethernet Adapter, and not Ethernet 4)
+        SERVER_PORT = 65432
 
+        # Create a socket object
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client_socket.connect((SERVER_IP, SERVER_PORT))
+        except socket.error as e:
+            print(f"Socket connection error: {e}")
+            sys.exit(1)
 
-    # Create a socket object
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((SERVER_IP, SERVER_PORT))
+        while True:
+            # Acquire Data
+            data1, data2 = acquire_data(rp_s)
+
+            # Detect Peaks
+            peaks1 = detect_peaks(data1, )
+            peaks2 = detect_peaks(data2)
+
+            # Calculate Time Difference
+            sample_differences = calculate_sample_differences(peaks1, peaks2)
+
+            # Error handeling if sample_difference returns None; if the sample_difference contintues to not be found, the program will exit
+            if sample_differences == None:
+                time.sleep(2)
+                for attempt in range(5):    # Adjust the number of retrys as needed
+                    data1, data2 = acquire_data(rp_s)
+                    peaks1 = detect_peaks(data1, )
+                    peaks2 = detect_peaks(data2)
+                    sample_differences = calculate_sample_differences(peaks1, peaks2)
+                    if sample_differences is not None:
+                        break
+                    time.sleep(1)
+            if sample_differences is None:
+                print("Failed to calculate sample differences after 5 attempts. Exiting program.")
+                sys.exit(1)
+
+            # Calculate Error Correction
+            feedback = calculate_error_correction(sample_differences, target_sample_diff, gain, feedback)
+
+            # Output the feedback voltage
+            voltage = output_feedback_voltage(rp_s, feedback)
+
+            # Prepare data to send
+            message = json.dumps({
+            'data1': data1.tolist(),  # Convert NumPy array to list
+            'peaks1': peaks1.tolist(),  # Convert NumPy array to list
+            'data2': data2.tolist(),  # Convert NumPy array to list
+            'peaks2': peaks2.tolist(),  # Convert NumPy array to list
+            'voltage' : voltage
+            })
+
+            # Encode the message and get its length
+            message_bytes = message.encode('utf-8')
+            message_length = len(message_bytes)
+
+            # Pack the message length into 4 bytes (Big Endian)
+            header = struct.pack('>I', message_length)
+
+            # Send the header followed by the message
+            client_socket.sendall(header + message_bytes)
+
+            #Rest
+            time.sleep(rest_time)
+
+    except KeyboardInterrupt:
+        print("Scanning stopped by user.")
+        sys.exit(0)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+    finally:
+        client_socket.close()
+
+else:
+    print("Server functionality is disabled.")
 
     while True:
-        # Acquire Data
-        data1, data2 = acquire_data(rp_s)
+            # Acquire Data
+            data1, data2 = acquire_data(rp_s)
 
-        # Detect Peaks
-        peaks1 = detect_peaks(data1, )
-        peaks2 = detect_peaks(data2)
+            # Detect Peaks
+            peaks1 = detect_peaks(data1, )
+            peaks2 = detect_peaks(data2)
 
-        # Calculate Time Difference
-        sample_differences = calculate_sample_differences(peaks1, peaks2)
+            # Calculate Time Difference
+            sample_differences = calculate_sample_differences(peaks1, peaks2)
 
-        # Calculate Error Correction
-        feedback = calculate_error_correction(sample_differences, target_sample_diff, gain, feedback)
+            # Error handeling if sample_difference returns None; if the sample_difference contintues to not be found, the program will exit
+            if sample_differences == None:
+                time.sleep(2)
+                for attempt in range(5):    # Adjust the number of retrys as needed
+                    data1, data2 = acquire_data(rp_s)
+                    peaks1 = detect_peaks(data1, )
+                    peaks2 = detect_peaks(data2)
+                    sample_differences = calculate_sample_differences(peaks1, peaks2)
+                    if sample_differences is not None:
+                        break
+                    time.sleep(1)
+            if sample_differences is None:
+                print("Failed to calculate sample differences after 5 attempts. Exiting program.")
+                sys.exit(1)
 
-        # Output the feedback voltage
-        voltage = output_feedback_voltage(rp_s, feedback)
+            # Calculate Error Correction
+            feedback = calculate_error_correction(sample_differences, target_sample_diff, gain, feedback)
 
-        # Prepare data to send
-        message = json.dumps({
-        'data1': data1.tolist(),  # Convert NumPy array to list
-        'peaks1': peaks1.tolist(),  # Convert NumPy array to list
-        'data2': data2.tolist(),  # Convert NumPy array to list
-        'peaks2': peaks2.tolist(),  # Convert NumPy array to list
-        'voltage' : voltage
-        })
+            # Output the feedback voltage
+            voltage = output_feedback_voltage(rp_s, feedback)
 
-        # Encode the message and get its length
-        message_bytes = message.encode('utf-8')
-        message_length = len(message_bytes)
-
-        # Pack the message length into 4 bytes (Big Endian)
-        header = struct.pack('>I', message_length)
-
-        # Send the header followed by the message
-        client_socket.sendall(header + message_bytes)
-
-        time.sleep(.5)  # Adjust as needed
-
-except KeyboardInterrupt:
-    print("Scanning stopped by user.")
-    sys.exit(0)
-except Exception as e:
-    print(f"An error occurred: {e}")
-    sys.exit(1)
-finally:
-    client_socket.close()
+            time.sleep(rest_time)
